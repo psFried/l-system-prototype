06f030e52c9ddebc01e97ad31ef6c3ce4495ce83
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-11-09 06:36:13 +0100

    Implement a simple draw function
    The turtle will not be able to push and pop state

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,40 @@ extern crate turtle;
 use turtle::Turtle;
 
 fn main() {
+    let word = vec![
+        Variable::F,
+        Variable::Minus,
+        Variable::F,
+        Variable::Plus,
+        Variable::Plus,
+        Variable::F,
+        Variable::Minus,
+        Variable::F
+    ];
+
     let mut turtle = Turtle::new();
+    turtle.set_heading(0.0);
 
-    for _i in 0..360 {
-        turtle.forward(3.0);
+    draw(&word, &mut turtle);
+}
 
-        turtle.right(1.0);
+enum Variable {
+    F,
+    Plus,
+    Minus,
+}
+
+type Word = Vec<Variable>;
+
+
+fn draw(word: &Word, turtle: &mut Turtle) {
+    for variable in word {
+        match variable {
+            Variable::F => { turtle.forward(100.0); }
+
+            Variable::Minus => { turtle.left(60.0); }
+
+            Variable::Plus => { turtle.right(60.0); }
+        }
     }
 }
